bf38338096372c6877a1414129ff930b9f9b9796
Michael Omonedo <rukkyomonedo@gmail.com>
2025-08-14 21:27:54 +0100

    initial commit

---
+++ mondilla-connect-landing/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/index.css'
+
+function App() {
+  return (
+    <>
+      <p className="bg-red-400">Hello world</p>
+    </>
+  );
+}
+
+export default App;
